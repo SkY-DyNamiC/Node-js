6f8bbe7924c96d37dda1a970ee70cb45a92927c2
SkY-DyNamiC <harsh832019@gmail.com>
2025-04-12 15:45:11 +0530

    Database login form

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 const express =require('express')
+const app = express();
 const mongoose =require('mongoose')
-
-const app =express()
+const path = require('path')
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 const PORT =4000
 
-app.get('/hello', (req, res)=>{
-    res.send("Hello the server is ready");
-    console.log("Hello")
+const userModel = require('./model/user')
+
+app.use(express.static(path.join(__dirname, 'auth')))
+
+mongoose.connect('mongodb://localhost:27017/project')
+.then(()=> console.log("Connected to the database."))
+.catch((err)=> console.log("Error connecting the database.", err))
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'auth', 'signup.html'))
 })
 
+app.get('/login', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'auth', 'login.html'))
+})
+
+app.post('/user', (req, res)=>{
+    userModel.create(req.body)
+    .then((data)=> res.json(data))
+    .catch((err)=> req.json(err))
+})
 
 
 app.listen(PORT, (req,res)=>{
